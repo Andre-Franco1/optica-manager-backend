41718f1535167d852906ff187818d43aa8dd3ec7
Andre-Franco1 <andrefranco3172@gmail.com>
2025-12-11 16:59:55 -0300

    first commit

---
+++ src/main/java/com/optica/manager/OpticamanagerApplication.java
@@ -0,0 +1,13 @@
+package com.optica.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticamanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticamanagerApplication.class, args);
+	}
+
+}
