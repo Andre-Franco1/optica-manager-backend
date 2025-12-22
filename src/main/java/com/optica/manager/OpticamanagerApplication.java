e34d78daffe9621d52b65eb593f7c83c23a4e9ca
Andre-Franco1 <andrefranco3172@gmail.com>
2025-12-22 15:36:22 -0300

    feat: add transactional and spring.jpa.open-in-view=false

--- src/main/java/com/optica/manager/OpticamanagerApplication.java
+++ src/main/java/com/optica/manager/OpticamanagerApplication.java
@@ -2,8 +2,10 @@ package com.optica.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class OpticamanagerApplication {
 
 	public static void main(String[] args) {
