e43cced949c6cc7ce0299d892b9cf29f3d0f4f26
Andre-Franco1 <andrefranco3172@gmail.com>
2025-12-26 16:26:49 -0300

    feat: frontend integration

--- src/main/java/com/optica/manager/domain/services/SaleService.java
+++ src/main/java/com/optica/manager/domain/services/SaleService.java
@@ -1,12 +1,15 @@
 package com.optica.manager.domain.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.optica.manager.domain.entities.Product;
 import com.optica.manager.domain.entities.Sale;
 import com.optica.manager.domain.entities.SaleItem;
+import com.optica.manager.domain.enums.SaleStatus;
 import com.optica.manager.domain.mappers.SaleMapper;
 import com.optica.manager.domain.repositories.ProductRepository;
 import com.optica.manager.domain.repositories.SaleRepository;
@@ -26,6 +29,13 @@ public class SaleService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public Page<SaleResponse> findPendingSales(int page, int size) {
+        var pageRequest = PageRequest.of(page, size);
+        var pageSale = saleRepository.findAllBySaleStatus(SaleStatus.PENDING, pageRequest);
+        return pageSale.map(s -> SaleMapper.toSaleResponseDTO(s));
+    }
     
     @Transactional
     public SaleResponse createSale(SaleRequest saleRequest) {
